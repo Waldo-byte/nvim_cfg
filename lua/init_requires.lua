3e1772ceb4aad9b21d43512144e6818688b4ca41
wm <wm@nanoteq.net>
2023-11-29 08:49:56 +0200

    Updated to account for svelte

--- lua/init_requires.lua
+++ lua/init_requires.lua
@@ -1,3 +1,6 @@
+local linux = 0
+local fsb = 1
+local operating_sys = linux
 -- plugins
 require("plugins.lazy")
 require('mason').setup()
@@ -8,3 +11,17 @@ require("plugin_config.treesitter_conf")
 -- settings
 require("options")
 require("keymaps")
+
+if operating_sys == fsb then
+  local nvim_clanglsp = require('lspconfig')
+  nvim_clanglsp.clangd.setup{
+  cmd = { 'clangd', '--background-index' },
+  filetypes = { 'c', 'cpp' },  -- Set the filetypes for which this language server should be enabled
+  root_dir = nvim_clanglsp.util.root_pattern('.git', 'compile_commands.json'),  -- Set the root directory detection method
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),  -- Add capabilities
+  on_attach = function(client, bufnr)
+    -- Custom on_attach function
+    -- You can add additional keybindings or configurations here
+  end,
+  }
+end
